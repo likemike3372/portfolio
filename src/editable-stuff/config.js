35e004b6732ba0451345ff24036d4b0f8c99f87e
mikth3372 <mikth@seas.upenn.edu>
2024-01-23 07:47:34 -0500

    updates

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -47,7 +47,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/mikhael.jpg"),
   imageSize: 375,
   message:
     "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
